3257fe99578fe660a1193ed4aef9738f1876ec20
huming2207 <huming2207@gmail.com>
2016-10-15 16:44:20 +1100

    Add a simple tester

---
+++ UrbanToApple/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace UrbanToApple
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Urban2Apple");
+			Console.WriteLine("Urban Dictionary to Apple Mac OSX Dictionary Converter");
+			Console.WriteLine("By Jackson Ming Hu @ RMIT University, 2016");
+			Console.WriteLine("=======================================================");
+			Console.WriteLine("This project is used for educational proposes only. \n" + 
+			                  "Please ALWAYS RESPECT the original and Urban Dictionary!" + 
+			                  "...and ALWAYS RESPECT/FOLLOW THE RELATED LAW!");
+			Console.WriteLine("=======================================================");
+		}
+	}
+}
